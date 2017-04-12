9c8a1ceb6fe0e9ba95ff873e4cf4623185702161
yathso <yatso@Yats-MacBook-Pro-2.local>
2017-04-12 14:05:01 -0700

    v1 John's original airbus code

---
+++ airbus.js
@@ -0,0 +1,9 @@
+var totalTickets = 555;
+var counterBuyTickets = prompt("There are 555 tickets, how many tickets you want to buy?");
+var balanceTickets = totalTickets - counterBuyTickets;
+
+if(counterBuyTickets === 555); {
+		alert("You have bought all tickets, there are no tickets left")
+	}else{
+		alert("You are buying " + counterBuyTickets + "!")
+}
